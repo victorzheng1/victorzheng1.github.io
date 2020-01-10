415706a026ea047ef944e1976dd048c4561f601f
Victor Zheng <50785942+victorzheng1@users.noreply.github.com>
2020-01-09 19:21:47 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 // JavaScript source code
 
-function fact_generate{
-    let facts = { "Big supporter of the Tottenham Hotspurs!", "Has never consumed alcohol or any perspective changing substances", "Never been more South than Virginia!"}
-    return facts[Math.random(facts.length-1)];
-}
 
